aaf2cb09565450d7397903522f87ad4dd6632dca
Rob Koch <rob.koch@live.com>
2015-10-21 20:30:30 -0600

    initial

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Delay(5000); // give it time for website to fire
+
+            RunAsync().Wait();
+
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+        }
+
+        static async Task RunAsync() {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:53402/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // New code:
+                HttpResponseMessage response = await client.GetAsync("Home/Get/1");
+                if (response.IsSuccessStatusCode)
+                {
+                    var vm = await response.Content.ReadAsAsync<SameViewModelButDifferentName>();
+                    Console.WriteLine("{0}|{1}|{2}", vm.Name, vm.State, vm.Url);
+                }
+
+                response = await client.GetAsync("Home/Get/2");
+                if (response.IsSuccessStatusCode)
+                {
+                    var vm = await response.Content.ReadAsAsync<SameViewModelButDifferentName>();
+                    Console.WriteLine("{0}|{1}|{2}", vm.Name, vm.State, vm.Url);
+                }
+
+                response = await client.GetAsync("Home/Get/3");
+                if (response.IsSuccessStatusCode)
+                {
+                    var vm = await response.Content.ReadAsAsync<SameViewModelButDifferentName>();
+                    Console.WriteLine("{0}|{1}|{2}", vm.Name, vm.State, vm.Url);
+                }
+            }
+        }
+    }
+
+    public class SameViewModelButDifferentName
+    {
+        public string State { get; set; }
+        public string Url { get; set; }
+        public string Name { get; set; } 
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public JsonResult Get(int id)
+        {
+            return Json(GetData(id), JsonRequestBehavior.AllowGet);
+        }
+
+        public ExternalLoginViewModel GetData(int id)
+        {
+            var externalLoginVM = new ExternalLoginViewModel
+            {
+                Name = "Home",
+                Url = "http://127.0.0.1",
+                State = "CO"
+            };
+
+            switch (id)
+            {
+                case 1:
+                    externalLoginVM = new ExternalLoginViewModel
+                    {
+                        Name = "Redmond",
+                        Url = "http://microsoft.com",
+                        State = "WA"
+                    };
+                    break;
+                case 2:
+                    externalLoginVM = new ExternalLoginViewModel
+                    {
+                        Name = "New York",
+                        Url = "http://mhfi.com",
+                        State = "NY"
+                    };
+                    break;
+                default:
+                    externalLoginVM = new ExternalLoginViewModel
+                    {
+                        Name = "Denver",
+                        Url = "http://denverdevday.eventday.com",
+                        State = "CO"
+                    };
+                    break;
+
+            }
+            return externalLoginVM;
+        }
+    }
+}
